9e6bb571d37c26ba4d9dbce3b98d9e96c9edaa66
javier <javier.arriero@erudine.com>
2015-05-02 13:02:58 +0100

    Moved java files

---
+++ src/main/java/com/zeda/javatest/Main.java
@@ -0,0 +1,16 @@
+package com.zeda.javatest;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//JDBCTest.test();
+		//C2_LinkedList.test();
+		//C3_Stack.test();
+		//C4_TreeGraph.test();
+		C5_Bit.test();
+	}
+
+}
