34930d9d77c54a0f7f476615266dbff7782e2e7c
javier <javier.arriero@erudine.com>
2015-05-02 13:03:24 +0100

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject examples "0.1"
+	:source-paths ["src/main/clj"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.namespace "0.2.7"]
+                 ])
